6e3fc0d9ce686407d9cee92901dffbddf1005ce3
Admin <admin@Admins-MacBook-Air.local>
2022-08-16 13:44:42 +0530

    mock interview project

--- main.js
+++ main.js
@@ -1,152 +1,197 @@
-let allColorBtn=document.querySelectorAll('.btn');
-let btnAll=document.getElementById('btn1');
-let btn2=document.getElementById('btn2');
-let btn3=document.getElementById('btn3');
-let btn4=document.getElementById('btn4');
-let btn6=document.getElementById('btn6');
-let btn7=document.getElementById('btn7');
+var create = document.getElementById("createtask")
+var main = document.getElementById("popcontainer")
+var mainpop = document.getElementById("popbox")
+var maincontainer = document.getElementById("checkbox")
+var inputvalue2 = document.getElementById("inputvalue")
+var check2 = document.getElementById("check2")
+var check1 = document.getElementById("check")
+var red1 = document.getElementById("red1")
+var blue1 = document.getElementById("blue1")
+var yellow1 = document.getElementById("yellow1")
+var black1 = document.getElementById("black1")
+var boxn = document.getElementById("box")
+var inputarea = document.getElementById("textarea")
+var all = document.querySelector("body")
+var countbox = document.querySelector("#countbox")
+var addBox = document.getElementById("mybox")
+var tablehead = document.getElementById("tablehead")
 
-let section=document.querySelector('section')
-// let cardbox=document.querySelector('.card')
+var totalbox = 0
+function doning() {
+    var count = 0;
 
-let mainClass=document.querySelector('.main')
-let wrapperClass=document.querySelector('.wrapper')
-let inputFiledClass=document.querySelector('inputFiled')
-let textArea=document.querySelector('textarea');
-let subject=document.getElementById('subject');
-let doneBtn=document.getElementById('done')
+    let box = document.createElement("div");
+    box.setAttribute("id", "box");
+    red1.onclick = function redborder() {
+        box.style.borderTop = "20px solid red";
+        box.classList.add("redfilter");
 
-let wrapperBtn=document.querySelector('wrapperBtn')
-let textareaColorRed=document.getElementById('textarea_color_red')
-let textareaColorYellow=document.getElementById('textarea_color_yellow')
-let textareaColorGreen=document.getElementById('textarea_color_green')
-let textareaColorBlack=document.getElementById('textarea_color_black')
-let crossButton=document.getElementById('crossBtn')
-let changeColor=document.querySelector('.changeColors');
-
-let taskAddedContainer=document.querySelector('.taskAddedContainer')
-let taskAdded=document.querySelector('.taskAdded')
-let headingTag=document.querySelector('h2')
-let lockBtn=document.getElementById('lock')
-
-let cardTitle=document.querySelector('.card-title')
-let card=document.querySelector('.card')
-let cardBody=document.querySelector('.card-body')
-let cardBox=document.querySelector('.cardbox')
-
-let iconsContainer=document.querySelector('.icons-container')
-let secondPart=document.querySelector('.second-part')
-let firstPart=document.querySelector('.first-part')
-firstPart.style.display='none'
-
-lockBtn.addEventListener('click',()=>{
-    if(firstPart.style.display==='none'){
-        firstPart.style.display='block'
-    }else{
-        firstPart.style.display='none'
     }
-})
-
-btn6.addEventListener('click',function(){
-    if(mainClass.style.display==='none'){
-        mainClass.style.display='block';
-    }else{
-        mainClass.style.display='none'
+    yellow1.onclick = function yellowborder() {
+        box.style.borderTop = "20px solid yellow";
+        box.classList.add("yellowfilter");
+        box.classList.remove("redfilter");
     }
-})
+    blue1.onclick = function blueborder() {
+        box.style.borderTop = "20px solid blue";
+        box.classList.add("bluefilter");
+        box.classList.remove("redfilter");
+        box.classList.remove("yellowfilter");
+    }
+    black1.onclick = function blackborder() {
+        box.style.borderTop = "20px solid black";
+        box.classList.add("blackfilter");
+        box.classList.remove("redfilter");
+        box.classList.remove("bluefilter");
+        box.classList.remove("yellowfilter");
+    }
+
+    let taskname = document.createElement("h2");
+    taskname.setAttribute("id", "taskname");
+    let inputvalue = document.getElementById("inputvalue").value;
+    taskname.textContent = inputvalue;
+    box.append(taskname);
+
+    let icons = document.createElement("div");
+    icons.classList.add("icons");
+    box.append(icons);
 
 
- let addCard=()=>{
-    // const titletext=document.getElementById('titletext');
-    let cardbox=document.querySelector('.cardbox')
-    textArea.addEventListener('click',()=>{
-            if(cardbox.style.display==='none'){
-                cardbox.style.display='block';
-            }else{
-                cardbox.style.display='none'
-            }
-        })
+    let action = document.createElement("div");
+    action.setAttribute("id", "actionicon");
+    action.classList.add("actionicon")
+    action.style.display = "none";
+    icons.append(action);
 
-    // created element
+    let check_icon = document.createElement("i");
+    check_icon.classList.add('fa', 'fa-check');
+    check_icon.setAttribute("id", "check2")
+    action.append(check_icon);
 
-    const newcard=document.createElement('div')
-    newcard.className='card'
-    
-    // textareaColorYellow.addEventListener('click',()=>{
-    //     newcard.style.backgroundColor='yellow'
-    //    })
-    //    textareaColorGreen.addEventListener('click',()=>{
-    //     newcard.style.backgroundColor='green'
-    //    })
-    //    textareaColorBlack.addEventListener('click',()=>{
-    //     .style.backgroundColor='black'
-    //    })
+    let edit_icon = document.createElement("i");
+    edit_icon.classList.add("fa", "fa-pencil");
+    edit_icon.setAttribute("id", "edit")
+    action.append(edit_icon);
 
-    const changecolors =document.createElement('div')
-    changecolors.className='changeColors';
+    let delete_icon = document.createElement("i");
+    delete_icon.classList.add('fa', 'fa-trash-o');
+    delete_icon.setAttribute("id", "delete")
+    action.append(delete_icon);
 
-    const newcardbody=document.createElement('div')
-    newcardbody.className='card-body';
+    let changecolor_icon = document.createElement("i");
+    changecolor_icon.classList.add('fa', 'fa-square');
+    changecolor_icon.setAttribute("id", "changecolors")
+    action.append(changecolor_icon);
 
-    const newh3=document.createElement('h3');
-    newh3.className='card-title';
-    newh3.innerText=textArea.value;
 
-    // const colors=document.createElement('div')
-    // colors.className=changeColor;
+    let lock_icons = document.createElement("div");
+    lock_icons.classList.add("lockicon");
+    icons.append(lock_icons);
 
-    // const newp=document.createElement('p')
+    let lock_icon = document.createElement("i");
+    lock_icon.classList.add('fa', 'fa-lock');
+    lock_icon.setAttribute("id", "lock");
+    lock_icons.append(lock_icon);
 
-    cardbox.appendChild(newcard)
-    newcard.appendChild(changecolors)
-    changecolors.appendChild(newcardbody);
-    newcardbody.appendChild(newh3);
-    
-    // card.style.width='35ch';
-    // card.style.backgroundColor='lightskyblue';
-    // card.style.borderRadius='5px';
-    // card.style.height='200px'
+    let unlock_icon = document.createElement("i");
+    unlock_icon.classList.add('fa', 'fa-unlock');
+    unlock_icon.setAttribute("id", "unlock");
+    unlock_icon.style.display = "none"
+    lock_icons.append(unlock_icon);
 
-    
+    maincontainer.append(box);
+
+    inputvalue2.value = "";
+
+    lock_icon.addEventListener("click", function lock() {
+        action.style.display = "block";
+        unlock_icon.style.display = "block";
+        lock_icon.style.display = "none";
+    });
+
+    unlock_icon.onclick = function unlock() {
+        action.style.display = "none";
+        unlock_icon.style.display = "none";
+        lock_icon.style.display = "block";
+    };
+
+
+    changecolor_icon.onclick = function changecolors() {
+        let colors = ["yellow", "blue", "black", "red"];
+        let cc = ""
+        box.style.borderColor = cc + colors[count];
+        changecolor_icon.style.color = cc + colors[count];
+        count++
+    }
+
+
+    edit_icon.onclick = function editTaskName() {
+        if (taskname.contentEditable != "true") {
+            taskname.contentEditable = "true"
+            edit_icon.style.color = "green"
+        }
+        else {
+            taskname.contentEditable = "false";
+            edit_icon.style.color = "white";
+        }
+    }
+
+
+
+    delete_icon.onclick = function remove() {
+        maincontainer.removeChild(box)
+        totalbox--;
+        countbox.textContent = totalbox;
+        if(totalbox==0){
+            alert("there is no box!")
+        }
+    }
+
+    check_icon.onclick = function checked() {
+        // box.style.display = "none";
+        tablehead.append(box);
+        tablehead.classList.add("tableheadbox");
+       
+
+
+    }
+
+
+    check1.onclick = function showchecked() {
+        check1.style.color = "green";
+        addBox.style.display = "block";
+        maincontainer.style.display="none";
+    }
+
+
+    totalbox++;
+    countbox.textContent = totalbox;
+
+
+
+
 }
 
-doneBtn.addEventListener('click',addCard);
-
-// let task = input.value;
-// if(task.length>0){
-//     let task_element=document.createElement('div');
-//     task_element.classList.add('first-part');
-//     iconsContainer.append(task_element);
-// }
-
-// let editbutton=document.createElement('button');
-// editbutton.classList.add('edit');
-// editbutton.innerText="\u{270F}";
-
-// task_element.append(editbutton);
+function show() {
+    main.style.display = "block";
+    maincontainer.style.opacity = ".3";
+}
 
 
-// let deleteBtn=document.createElement('button');
-// deleteBtn.classList.add('delete');
-// deleteBtn.innerText="\u{2421}";
+function closing() {
+    if (main.style.display = "block") {
+        main.style.display = "none";
+        maincontainer.style.opacity = "1";
+    }
 
-// task_element.append(deleteBtn)
-// input.value=""
-
-// editbutton.addEventListener('click',()=>{l
-//     if(editbutton.innerText=="\u{270F}"){
-//         editbutton.innerText="\u{270F}"
-//         cardTitle.removeAttribute('readonly')
-//     }else{
-//         editbutton.innerText="\u{270F}"
-//         cardTitle.removeAttribute("readonly",readonly)
-//     }
-// })
-
-// deleteBtn.addEventListener("click",()=>{
-//     card.removeChild(task_element)
-// })
+}
 
 
 
+console.log(boxn);
 
+function showall(){
+    maincontainer.style.display="flex";
+    addBox.style.display="none";
+
+}
